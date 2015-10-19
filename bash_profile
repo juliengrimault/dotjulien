32cb328a050da55bb73f83c49328f25efc2c7da3
Julien Grimault <julien_grimault@apple.com>
2015-10-19 10:22:52 -0700

    SxS setup in Maps

---
+++ bash_profile
@@ -0,0 +1,14 @@
+#path to your source folder
+SRC_DIR=/Volumes/Data/Users/julien/src/
+
+#path to the MapsStack folder, which should be a top level folder in your src tree
+MAPS_STACK_DIR=$SRC_DIR/MapsStack/
+
+#path to where SxS Xcodes should be installed
+SXS_XCODE_DIR=/Applications/Xcode/
+
+#your OD username
+OD_USERNAME=julien_grimault
+
+#load our helper functions
+source $SRC_DIR/MapsTools/MapsTools.bash
---
+++ zsh/maps.zsh
@@ -0,0 +1,21 @@
+#Maps
+alias rsync_geoservices='ppy rrsync /var/mobile/Library/Caches/GeoServices'
+alias rsync_maps='python $SRC_DIR/tools/rsync_maps.py'
+
+#zsh support
+setopt shwordsplit
+
+#path to your source folder
+export SRC_DIR=/Volumes/Data/Users/julien/src
+
+#path to the MapsStack folder, which should be a top level folder in your src tree
+export MAPS_STACK_DIR=$SRC_DIR/MapsStack/
+
+#path to where SxS Xcodes should be installed
+export SXS_XCODE_DIR=/Applications/Xcode/
+
+#your OD username
+OD_USERNAME=julien_grimault
+
+#load our helper functions
+source $SRC_DIR/MapsTools/MapsTools.bash
