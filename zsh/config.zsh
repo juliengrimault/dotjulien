175f05a874a77d7e15c9466e9868632e1acc6f0e
Julien Grimault <julien_grimault@apple.com>
2015-11-13 23:54:21 -0800

    Add JAVA_HOME env variable

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -9,6 +9,8 @@ export GIT_DIFF_OPTS=-U999999
 export PATH="./bin:/Applications/Postgres93.app/Contents/MacOS/bin:$HOME/.bin:$HOME/.rbenv/plugins/ruby-build/bin:/usr/local/sbin:/usr/local/heroku/bin:/usr/local/foreman/bin:/usr/local/bin:$PATH:$HOME/.pulse/bin:$HOME/.dotjulien/git:$HOME/.bin/arcanist/bin:$HOME/.cabal/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
+export JAVA_HOME="`/usr/libexec/java_home -v 1.7`"
+
 # Timer
 REPORTTIME=10 # print elapsed time when more than 10 seconds
 
