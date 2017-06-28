ef2b0f586200fd11ca2ce664b396aa65966f0548
Julien Grimault <julien_grimault@apple.com>
2017-06-28 09:33:09 -0700

    update mapstools config

--- bash_profile
+++ bash_profile
@@ -1,1 +1,5 @@
-source ~/.mapstools_profile
+# Load our shell agnostic profile
+source /Volumes/Data/Users/$(who am i | awk '{print $1}')/.mapstools_profile
+
+# Load SxS Scripts
+source "${SRC_DIR}"MapsTools/MapsTools.bash
--- zsh/maps.zsh
+++ zsh/maps.zsh
@@ -5,4 +5,6 @@ alias rsync_maps='python $SRC_DIR/tools/rsync_maps.py'
 #zsh support
 setopt shwordsplit
 
-source ~/.mapstool_profile
+
+source /Volumes/Data/Users/$(who am i | awk '{print $1}')/.mapstools_profile
+source "${SRC_DIR}"/MapsTools/MapsTools.bash
